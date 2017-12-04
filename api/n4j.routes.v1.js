59eb4c6344c074268b50e0a0fcca61979aa3e9f3
Imottall <tjbuiten@avans.nl>
2017-12-05 00:41:00 +0100

    Replaced the neo4j way of connecting with neo4j-driver in the hopes of fixing this piece of dingo

--- api/n4j.routes.v1.js
+++ api/n4j.routes.v1.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var routes = express.Router();
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase("https://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24780");
-
+//var neo4j = require('neo4j');
+//var db = new neo4j.GraphDatabase("https://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24780");
+var neo4j = require('neo4j-driver').v1;
+var driver =
+    neo4j.driver("bolt://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24786",
+    neo4j.auth.basic("admin", "b.gj4kU3HhDiFG.hhoE5SZipR0xvYXl"));
 
 routes.get('/neo/users', function(req, res) {
-    db.cypher({
+    /*db.cypher({
         query: 'CREATE (n:Person {name: {personName}}) RETURN n',
         params: {
             personName: 'Bob'
@@ -17,7 +20,20 @@ routes.get('/neo/users', function(req, res) {
         } else {
             console.log('Node saved to database with id:', result['n']['_id']);
         }
-    });
+    });*/
+    var session = driver.session();
+    session
+        .run("CREATE (n:Person {name:'Bob'}) RETURN n.name")
+        .then(function(result) {
+            result.records.forEach(function(record) {
+                console.log(record)
+            });
+
+            session.close();
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 });
 
 module.exports = routes;
