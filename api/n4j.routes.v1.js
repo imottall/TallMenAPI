e211828bb8ab57fd55689517127a6fba1ea50e01
Imottall <tjbuiten@avans.nl>
2017-12-04 19:56:03 +0100

    Updated the way we connect to our neodb

--- api/n4j.routes.v1.js
+++ api/n4j.routes.v1.js
@@ -1,37 +1,39 @@
 var express = require('express');
 var routes = express.Router();
-var neo4j = require('neo4j-driver').v1;
-var driver = neo4j.driver("bolt://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24786", neo4j.auth.basic("admin", "b.gj4kU3HhDiFG.hhoE5SZipR0xvYXl"));
-var session = driver.session();
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase("https://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24780");
 
 
 routes.get('/neo/users', function(req, res) {
-    session
-        .run("MATCH (n) RETURN n")
-        .then(function(result) {
-            result.records.forEach(function(record) {
-                console.log(JSON.stringify(record, null, 4))
-            });
-            session.close();
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+    db.cypher({
+        query: 'CREATE (n:Person {name: {personName}}) RETURN n',
+        params: {
+            personName: 'Bob'
+        }
+    }, function(err, results){
+        var result = results[0];
+        if (err) {
+            console.error('Error saving new node to database:', err);
+        } else {
+            console.log('Node saved to database with id:', result['n']['_id']);
+        }
+    });
 });
 
 routes.post('/neo/newuser', function(req, res) {
-    session
-        .run("CREATE (n:Person {name:'Bob'}) RETURN n.name")
-        .then(function(result) {
-            result.records.forEach(function(record) {
-                console.log(record)
-            });
-
-            session.close();
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+    db.cypher({
+        query: 'CREATE (n:Person {name: {personName}}) RETURN n',
+        params: {
+            personName: 'Bob'
+        }
+    }, function(err, results){
+        var result = results[0];
+        if (err) {
+            console.error('Error saving new node to database:', err);
+        } else {
+            console.log('Node saved to database with id:', result['n']['_id']);
+        }
+    });
 });
 
 module.exports = routes;
