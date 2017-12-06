9ea168adbc0edb07533dbe6c09e643aad6e9f24e
Imottall <tjbuiten@avans.nl>
2017-12-06 17:15:30 +0100

    Last fix worked, replaced the test neo4j route with a /games route which will return all the games in the database

--- api/n4j.routes.v1.js
+++ api/n4j.routes.v1.js
@@ -1,44 +1,25 @@
 var express = require('express');
 var routes = express.Router();
-//var neo4j = require('neo4j');
-//var db = new neo4j.GraphDatabase("https://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24780");
 var neo4j = require('neo4j-driver').v1;
 var driver =
     neo4j.driver("bolt://hobby-npihgclecffdgbkehgonlial.dbs.graphenedb.com:24786",
     neo4j.auth.basic("admin", "b.gj4kU3HhDiFG.hhoE5SZipR0xvYXl"));
 
-routes.get('/neousers', function(req, res) {
-    /*db.cypher({
-        query: 'CREATE (n:Person {name: {personName}}) RETURN n',
-        params: {
-            personName: 'Bob'
-        }
-    }, function(err, results){
-        var result = results[0];
-        if (err) {
-            console.error('Error saving new node to database:', err);
-        } else {
-            console.log('Node saved to database with id:', result['n']['_id']);
-        }
-    });*/
-    console.log("hey");
-
-    var session = driver.session();
-    console.log("we have a session");
-    session
-        .run("CREATE (n:Person {name:'Bob'}) RETURN n.name")
-        .then(function(result) {
-            result.records.forEach(function(record) {
-                console.log(record)
-            });
-            res.status(200).json(result);
-
-            session.close();
-        })
-        .catch(function(error) {
-            res.status(418).json(error);
-            console.log("meh: " + error);
-        });
+routes.get('/games', function(req, res){
+   var session = driver.session();
+   session
+       .run("MATCH (g:Game) RETURN g")
+       .then(function(result) {
+           result.records.forEach(function(record){
+               console.log(record)
+           });
+           res.status(200).json(result);
+           session.close();
+       })
+       .catch(function(error){
+           console.log("Error: " + error);
+           res.status(418).json(error);
+       })
 });
 
 module.exports = routes;
