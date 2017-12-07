f4b21747637f8908051625de37f02d087a7ce726
Imottall <tjbuiten@avans.nl>
2017-12-07 19:41:12 +0100

    Added a routes.get /:id/posts route

--- api/forum.routes.js
+++ api/forum.routes.js
@@ -4,7 +4,7 @@ var mongodb = require('../config/mongo.db');
 var Forum = require('../model/forum.model');
 
 /**
- * Geef een lijst van alle forums
+ * Returns all the forums
  */
 routes.get('/forums', function(req, res) {
     res.contentType('application/json');
@@ -13,8 +13,16 @@ routes.get('/forums', function(req, res) {
     .catch((error) => res.status(401).json(error));
 });
 
+routes.get('/:id/posts', function(req,res) {
+    const forumId = req.params.id;
+
+    Forum.findOne({_id: forumId})
+        .then((posts) => res.status(200).json(posts))
+        .catch((error) => res.status(400).json(error));
+});
+
 /**
- * Plaats een nieuwe post
+ * Add a new post to a specific forum
  */
 routes.post('/forums/:id', function(req, res, next) {
     const forumID = req.params.id;
@@ -27,5 +35,6 @@ routes.post('/forums/:id', function(req, res, next) {
     .then(forum => res.send(forum))
     .catch((error) => res.status(400).json(error))
 });
+
 
 module.exports = routes;
