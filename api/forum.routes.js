d53b9e3e648e37a1bf3eefd98fd97fc2be0da958
Imottall <tjbuiten@avans.nl>
2017-12-07 19:23:14 +0100

    added a post new 'post' route

--- api/forum.routes.js
+++ api/forum.routes.js
@@ -3,24 +3,29 @@ var routes = express.Router();
 var mongodb = require('../config/mongo.db');
 var Forum = require('../model/forum.model');
 
-//
-// Geef een lijst van alle users.
-//
+/**
+ * Geef een lijst van alle forums
+ */
 routes.get('/forums', function(req, res) {
     res.contentType('application/json');
     Forum.find({})
-        .then((forums) => {
-        // console.log(users);
-        res.status(200).json(forums);
-    })
+        .then((forums) => res.status(200).json(forums))
     .catch((error) => res.status(401).json(error));
 });
 
-routes.post('/forum', function(req, res, next) {
-    const forumReq = req.body;
-    Recipe.create(forumReq)
-        .then(forum => res.status(200).send(forum))
-        .catch((error) => res.status(400).json(error))
+/**
+ * Plaats een nieuwe post
+ */
+routes.post('/forums/:id', function(req, res, next) {
+    const forumID = req.params.id;
+    const newPost = req.body;
+
+    Forum.update(
+        {_id: forumID},
+        { $addToSet: { posts: [newPost] }}
+    )
+    .then(forum => res.send(forum))
+    .catch((error) => res.status(400).json(error))
 });
 
 module.exports = routes;
