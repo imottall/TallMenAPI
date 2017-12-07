8c91760fcc60e4c44a230737c524cf03f50abad9
Imottall <tjbuiten@avans.nl>
2017-12-07 14:40:14 +0100

    Added a forum route

---
+++ model/forum.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ForumSchema = new Schema({
+    forums: [{
+        topic: {
+            type: String,
+            required: true
+        },
+        posts: [{
+            title: {
+                type: String,
+                required: true
+            },
+            author: {
+                type: String,
+                required: false
+            },
+            message: {
+                type: String,
+                required: false
+            },
+            replies: [{
+                message: {
+                    type: String,
+                    required: true
+                },
+                author: {
+                    type: String,
+                    required: false
+                }
+            }]
+        }]
+    }]
+}, {
+    timestamps: true
+});
+
+
+const Forum = mongoose.model('forum', ForumSchema);
+
+// Add a 'dummy' user (every time you require this file!)
+const forum = new Forum({
+    topic: 'dummy'
+}).save();
+
+module.exports = Forum;
