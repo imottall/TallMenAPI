f29c6613572f22abc7643f5c9b4685c1c38a2c42
Imottall <tjbuiten@avans.nl>
2017-12-10 19:51:57 +0100

    Potential fix coming in

--- model/forum.model.js
+++ model/forum.model.js
@@ -41,8 +41,7 @@ const Forum = mongoose.model('forum', ForumSchema);
 // Add a 'dummy' user (every time you require this file!)
 const forum = new Forum({
         topic: 'dummy', posts:
-        [{title: 'Im bloody amazing', author: 'moi', message: "I'm amazing because, you know, that's just what I am", replies:
-            [{message: 'I agree', author: 'toi'},{message: 'totally you know', replies: [{message: 'go away'},{message: '16 year old scrub'}]}]
+        [{title: 'Im bloody amazing', author: 'moi', message: "I'm amazing because, you know, that's just what I am"
         }]
 }).save();
 
