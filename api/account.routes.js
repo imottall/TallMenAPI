6228ba6285888954083d3f19d44cec990cde87bd
Imottall <tjbuiten@avans.nl>
2017-12-11 17:57:46 +0100

    last fix worked, now making sure that we get a response

--- api/account.routes.js
+++ api/account.routes.js
@@ -6,7 +6,7 @@ var Account = require('../model/account.model');
 routes.post('/login', function(req, res, next) {
     const account = req.body;
 
-    Account.find({name: account.name}, {password: account.password})
+    Account.find({name: account.name}, {password: account.password}, {name: 1, password: 1})
         .then(response => res.status(200).send(response))
         .catch((error) => res.status(400).json(error))
 });
