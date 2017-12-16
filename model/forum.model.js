b173547271b30d0da8862225afae9862b0576544
Imottall <tjbuiten@avans.nl>
2017-12-16 17:25:08 +0100

    Looks like the fix is gonna take a while, trying again

--- model/forum.model.js
+++ model/forum.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Account = require('./account.model');
+
+const Post = require('post.model');
 
 const ForumSchema = new Schema({
     topic: {
@@ -13,32 +14,6 @@ const ForumSchema = new Schema({
     }]
 });
 
-const PostSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String,
-        required: false
-    },
-    account: { type: Schema.Types.ObjectId, ref: 'Account' },
-    replies: [{ type: Schema.Types.ObjectId, ref: 'Reply' }]
-});
-
-const ReplySchema = new Schema({
-    message: {
-        type: String,
-        required: true
-    },
-    account: { type: Schema.Types.ObjectId, ref: 'Account' },
-    replyToAuthor: { type: Schema.Types.ObjectId, ref: 'Account' }
-});
-
 const Forum = mongoose.model('forum', ForumSchema);
-const Post = mongoose.model('post', PostSchema);
-const Reply = mongoose.model('reply', ReplySchema);
 
 module.exports = Forum;
-module.exports = Post;
-module.exports = Reply;
--- model/post.model.js
+++ model/post.model.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Account = require('./account.model');
-const Reply = require('./reply.model');
+const Account = require('account.model');
+const Reply = require('reply.model');
 
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: false
+    },
+    account: { type: Schema.Types.ObjectId, ref: 'Account' },
+    replies: [{ type: Schema.Types.ObjectId, ref: 'Reply' }]
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+module.exports = Post;
