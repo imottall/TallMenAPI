a9a0566b1f7e3b77ea11af6509cd8c4073db0cd5
Imottall <tjbuiten@avans.nl>
2017-12-16 17:57:45 +0100

    Finally fixed this sh*t

--- api/forums.routes.js
+++ api/forums.routes.js
@@ -15,6 +15,7 @@ var Replies = require('../model/reply.model');
 routes.get('/forums', function(req, res) {
     res.contentType('application/json');
     Forums.find({})
+        .populate('posts')
         .then((response) => res.status(200).json(response))
         .catch((error) => res.status(401).json(error));
 });
@@ -34,7 +35,7 @@ routes.post('/forums/:forumId', function(req,res){
     const forumId = req.params.forumId;
     const newForum = req.body;
 
-    Replies.findOneAndUpdate({_id: forumId}, newForum)
+    Forums.findOneAndUpdate({_id: forumId}, newForum)
         .then(response => res.status(200).send(response))
         .catch((error) => res.status(400).json(error))
 });
@@ -56,6 +57,7 @@ routes.delete('/forums/:forumId', function(req, res, next) {
 routes.get('/:forumId/posts', function(req, res) {
     res.contentType('application/json');
     const forumId = req.params.forumId;
+
     Forums.findOne({_id: forumId})
         .populate('posts')
         .then((response) => res.status(200).json(response))
@@ -89,6 +91,52 @@ routes.delete('/posts/:postId', function(req, res, next) {
     const postId = req.params.postId;
 
     Posts.remove({ _id: postId})
+        .then((response) => res.status(200).json(response))
+        .catch((error) => res.status(400).json(error));
+});
+
+/*********
+ * REPLIES *
+ *********/
+
+//GET
+routes.get('/:postId/replies', function(req, res) {
+    res.contentType('application/json');
+    const postId = req.params.postId;
+
+    Posts.findOne({_id: postId})
+        .populate('replies')
+        .then((response) => res.status(200).json(response))
+        .catch((error) => res.status(401).json(error));
+});
+
+//NEW
+routes.post('/:postId/replies', function(req, res, next) {
+    const postId = req.params.postId;
+    newReply = new Replies(req.body);
+
+    Promise.all([
+        new Replies(newReply).save(),
+        Posts.findOneAndUpdate({_id: postId},{ $push: { replies: newReply }})
+    ])  .then(response => {res.status(200).send(response)})
+        .catch((error) => res.status(400).json(error))
+});
+
+//UPDATE
+routes.post('/replies/:replyId', function(req,res){
+    const replyId = req.params.replyId;
+    const newReply = req.body;
+
+    Replies.findOneAndUpdate({_id: replyId}, newReply)
+        .then(response => res.status(200).send(response))
+        .catch((error) => res.status(400).json(error))
+});
+
+//DELETE
+routes.delete('/replies/:replyId', function(req, res, next) {
+    const replyId = req.params.replyId;
+
+    Replies.remove({ _id: replyId})
         .then((response) => res.status(200).json(response))
         .catch((error) => res.status(400).json(error));
 });
